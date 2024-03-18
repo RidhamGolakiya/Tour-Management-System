522cff8e5c387dcd8bcbdc619cd417b020135cd7
Ridham Golakiya <ridhamgolakiya7878@gmail.com>
2024-03-18 16:25:13 +0530

    fix: open modal

--- package-details.php
+++ package-details.php
@@ -27,9 +27,33 @@ if (isset($_GET['packageId'])) {
     <link rel="stylesheet" href="./assets/bootstrap/bootstrap.min.css">
     <link rel="stylesheet" href="./assets/css/style.css">
     <link rel="stylesheet" href="./assets/font-awesome/css/all.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <link href="./assets/toastr/toaster.min.css" rel="stylesheet">
     <script src="./assets/toastr/toastr.min.js"></script>
     <style>
+
+        .tool_tip:hover .pricetooltip {
+            display: block;
+        }
+
+        .pricetooltip {
+            display: none;
+            background: black !important;
+            margin-left: 28px;
+            padding: 10px;
+            position: absolute;
+            z-index: 1000;
+            width: 200px;
+            font-size: 12px;
+        }
+
+        .tool_tip {
+            background: black !important;
+            color: white !important;
+            border-radius: 48%;
+            padding: 0px 4px;
+            text-align: center;
+        }
         /* Custom CSS */
         .carousel-container {
             position: relative;
@@ -183,6 +207,54 @@ if (isset($_GET['packageId'])) {
         <?php echo $row['other_details']; ?>
     </div>
 
+    <div class="container-fluid py-0">
+        <div class="modal fade" id="packageModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-xl">
+                <div class="modal-content custom-radius">
+                    <div class="text-center">
+                        <div>
+                            <button type="button" class="close btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            <h3 class="modal-title"><?php echo $name; ?></h3>
+                        </div>
+                        <h2 class="custom-price" style="margin-top: 15px;font-size: 30px;">₹<?php echo $formattedPrice; ?> <small>/ Per Person</small></h2>
+                    </div>
+                    <hr>
+                    <form autocomplete="off" action="./queries.php" method="POST">
+                        <div class="modal-body p-4 row">
+                            <input type="hidden" name="package_id" value="<?php echo $tour_id; ?>">
+                            <input type="hidden" name="package_price" value="<?php echo $price; ?>">
+                            <input type="hidden" name="package_name" value="<?php echo $name; ?>">
+                            <input type="hidden" name="total_price" id="grand_total">
+                            <div class="col-md-6">
+                                <label>Total Person: </label><span class="text-danger">*</span><span class="mx-1 tool_tip">?<span class="pricetooltip">Price will be multiply by total person you entered.</span></span>
+                                <input type="number" name="total_person" id="total_person" min="0" class="form-control mt-2 mb-3" placeholder="Total Person">
+                            </div>
+                            <div class="col-md-6">
+                                <label>Message: </label><span class="text-danger">*</span>
+                                <textarea class="form-control mt-2 mb-3" aria-invalid="false" name="message" id="msg" placeholder="Enter your message"></textarea>
+                            </div>
+                            <div class="col-md-6">
+                                <label>Payment Method:</label>
+                                <select name="payment_method" id="payment_method" class="form-control mt-2 mb-3">
+                                    <option value="manual">Manually</option>
+                                    <option value="strp">Pay with Stripe</option>
+                                </select>
+                            </div>
+                            <div class="col-md-6">
+                                <label>Total Price:</label>
+                                <label class="fw-bolder" id="total_price"></label>
+                            </div>
+                            <span id="validateMsg" class="text-danger"></span>
+                            <div class="col-md-12 mt-4 d-flex justify-content-center">
+                                <!-- <button name="payment" id="book" class="btn default-btn">Book</button> -->
+                                <button type="submit" name="payment" value="btnPayment" class="btn default-btn">Pay Now <span class="fa fa-angle-right"></span></button>
+                            </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <?php include_once "./components/footerHome.php"; ?>
     <script>
         $(document).ready(function() {
@@ -195,10 +267,30 @@ if (isset($_GET['packageId'])) {
                 $('#carouselExampleIndicators').carousel('next');
             });
         });
+        $(document).ready(function() {
+            function updateTotalPrice() {
+                var totalPersonInput = $("#total_person").val();
+                var price = <?php echo $price; ?>
+
+                // Check if the input is empty or not a valid number
+                if (totalPersonInput === "" || isNaN(totalPersonInput) || totalPersonInput < 0) {
+                    totalPersonInput = 0;
+                }
+
+                const formattedPrice = new Intl.NumberFormat("en-IN").format(price * totalPersonInput);
+                $("#total_price").text(`₹ ${formattedPrice}`);
+                $("#grand_total").val(formattedPrice);
+            }
+
+            // Update total price when input changes
+            $("#total_person").on("input", updateTotalPrice);
+
+            updateTotalPrice();
+        });
     </script>
 
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
     <script src="./assets/js/script.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
 </body>
